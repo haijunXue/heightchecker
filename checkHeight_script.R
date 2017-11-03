b90a1a2eab164b368933061f1f2d407ea7d1a25a
haijunXue <xuehaijun793g@gmail.com>
2017-11-03 16:57:01 +0100

    version vergleich

--- checkHeight_script.R
+++ checkHeight_script.R
@@ -20,62 +20,6 @@ students$name = c("Maria", "Franz", "Peter", "Lisa", "Hans", "Eva", "Mia", "Karl
 
 
 
-checkHeight3 = function(students.input = students){
-
-  result.frame = data.frame(matrix(NA, nrow = nrow(students.input), ncol = 2))
-  colnames(result.frame) = c("name", "difference")
-
-  male.mean = students.input %>%
-    filter(sex == "M") %>%
-    summarise(mean = mean(height))
-  female.mean = students.input %>%
-    filter(sex == "F") %>%
-    summarise(mean = mean(height))
-
-  for (i in 1:nrow(students.input)) {
-    # calculate sex-specific deviations from the mean
-    if (students.input[i, "sex"] == "F") {
-      height.diff = 100*(students.input[i,]$height - female.mean$mean)
-    }
-    else {
-      height.diff = 100*(students.input[i, ]$height - male.mean$mean)
-    }
-    result.frame[i, "name"] = as.character(students.input[i, "name"])
-    result.frame[i, "difference"] = height.diff
-  }
-  return(result.frame)
-}
-
-checkHeight3(students.input = students)
-
-
-
-
-
-#version 
-check_height_2 <- function(row){
-  #get correct gender name
-  if(row[['sex']] == 'M'){
-    gender = 'man'
-  }else{
-    gender = 'woman'
-  }
-  # Get gender mean height
-  m_height = df %>%
-    filter(sex == row[['sex']]) %>%
-    summarise(mean(height))
-  # get difference from mean
-  x = round((row[['height']]-m_height)*100,digits = 2)[1,1]
-  if (x >= 0) {
-    print(paste0(row[['names']],' is ',x,' cm taller than the average ',gender))
-  } else {
-    print(paste0(row[['names']],' is ',abs(x),' cm smaller than the average ', gender))
-  }
-  
-}
-check_height_2(df[2,])
-
-#
 check_height_3 <- function(df){
   l <- c()
   for(i in 1:nrow(df)){
@@ -85,4 +29,8 @@ check_height_3 <- function(df){
   
 }
 check_height_3(df)
+
+
+
+
 
